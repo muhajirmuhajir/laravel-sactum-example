d782d7df362d98dc26ddf4591ad38e6d9afff2cd
muhajirmuhajir <hajir.nf@gmail.com>
2021-04-04 20:41:13 +0800

    add login & logout feature

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -26,5 +27,40 @@ class AuthController extends Controller
             'user' => $user,
             'token' =>  $token
         ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:6'
+        ]);
+
+        // check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // check passwords
+
+        if (!$user || !Auth::attempt($fields)) {
+            return response([
+                'message' => 'invalid email or password',
+            ], 401);
+        };
+
+        $token = $user->createToken('myApptoken')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' =>  $token
+        ], 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response([
+            'message' => 'logged out',
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,10 @@ use App\Http\Controllers\ProductController;
 */
 
 Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function(){
     Route::post('products',[ProductController::class, 'store']);
+
+    Route::post('logout', [AuthController::class, 'logout']);
 });
